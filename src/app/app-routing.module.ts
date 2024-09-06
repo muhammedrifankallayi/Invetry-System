8413e9e65939920ef76119510cdc121cae2570db
muhammed rifan <rifanmuhammed173@gmail.com>
2024-09-06 22:50:00 +0530

    f c

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './default-layout/default-layout.component';
+
+const routes: Routes = [
+
+  {path:'',component:DefaultLayoutComponent,
+    children:[
+      
+    ]
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
